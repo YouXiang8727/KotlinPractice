051f346edd0ced24898ca6f05fc300d10f6b166e
youxiang <10546011@ntub.edu.tw>
2022-06-14 10:22:54 +0800

    no message

---
+++ app/src/main/java/com/youxiang8727/kotlinpractice/CoroutineTestActivity.kt
@@ -0,0 +1,11 @@
+package com.youxiang8727.kotlinpractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CoroutineTestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coroutine_test)
+    }
+}
